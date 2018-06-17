60ab22830564a0eda58facb92d651f3cfed77c5b
Francois Wouts <f@zenc.io>
2018-06-18 06:36:01 +1000

    Add basic tests for Swagger, OpenAPI and JSON Schema generation

---
+++ spec/apigen/formats/example.rb
@@ -0,0 +1,94 @@
+require 'apigen/rest'
+
+module Apigen
+  ##
+  # Generates an example API with a variety of endpoints and types.
+  def self.example
+    api = Apigen::Rest::Api.new
+
+    api.endpoint :list_users do
+      method :get
+      path "/users"
+      output :success do
+        status 200
+        type :array do
+          type :user
+        end
+      end
+    end
+
+    api.endpoint :create_user do
+      method :post
+      path "/users"
+      input :object do
+        name :string
+        email :string
+        password :string
+        captcha :string
+      end
+      output :success do
+        status 200
+        type :user
+      end
+      output :failure do
+        status 401
+        type :string
+      end
+    end
+
+    api.endpoint :update_user do
+      method :put
+      path "/users/{id}" do
+        id :string
+      end
+      input :object do
+        name :string?
+        email :string?
+        password :string?
+        captcha :string
+      end
+      output :success do
+        status 200
+        type :user
+      end
+      output :failure do
+        status 401
+        type :string
+      end
+    end
+
+    api.endpoint :delete_user do
+      method :delete
+      path "/users/{id}" do
+        id :string
+      end
+      output :success do
+        status 200
+        type :void
+      end
+      output :failure do
+        status 401
+        type :string
+      end
+    end
+
+    api.model :user do
+      type :object do
+        id :int32
+        profile :user_profile
+      end
+    end
+
+    api.model :user_profile do
+      type :object do
+        name :string
+        avatar_url :string
+      end
+    end
+
+    # Ensure that the API spec is valid.
+    api.validate
+
+    api
+  end
+end
---
+++ spec/apigen/formats/jsonschema_spec.rb
@@ -0,0 +1,48 @@
+require 'apigen/formats/jsonschema'
+require 'apigen/formats/example'
+require 'json'
+
+describe Apigen::Formats::JsonSchema::Draft7 do
+  it "generates expected output" do
+    generated = JSON.load(Apigen::Formats::JsonSchema::Draft7.generate Apigen::example)
+    expect(generated).to eq(JSON.load <<~JSON
+{
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "definitions": {
+    "user": {
+      "type": "object",
+      "properties": {
+        "id": {
+          "type": "integer",
+          "format": "int32"
+        },
+        "profile": {
+          "$ref": "#/definitions/user_profile"
+        }
+      },
+      "required": [
+        "id",
+        "profile"
+      ]
+    },
+    "user_profile": {
+      "type": "object",
+      "properties": {
+        "name": {
+          "type": "string"
+        },
+        "avatar_url": {
+          "type": "string"
+        }
+      },
+      "required": [
+        "name",
+        "avatar_url"
+      ]
+    }
+  }
+}
+JSON
+    )
+  end
+end
